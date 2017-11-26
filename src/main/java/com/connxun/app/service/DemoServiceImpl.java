b85ba8b7513df6e310c554f94e69ab06666c1531
soonphe <310869749@qq.com>
2017-11-26 14:40:14 +0800

    提交war包

---
+++ src/main/java/com/connxun/app/service/DemoServiceImpl.java
@@ -0,0 +1,9 @@
+package com.connxun.app.service;
+
+/**
+ * @Author anna
+ * @Date 2017-10-31 16:01
+ * @Description
+ */
+public class DemoServiceImpl {
+}
---
+++ src/main/java/com/connxun/app/service/IDemoService.java
@@ -0,0 +1,9 @@
+package com.connxun.app.service;
+
+/**
+ * @Author anna
+ * @Date 2017-10-31 15:57
+ * @Description
+ */
+public class IDemoService {
+}
